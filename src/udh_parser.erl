1d7972fb6385fdb072ec721a0c721b9d19d12226
Bikram Chatterjee <razorpeak@gmail.com>
2018-08-10 12:27:18 +0200

    #1 to verify

--- src/udh_parser.erl
+++ src/udh_parser.erl
@@ -6,6 +6,8 @@
 %%====================================================================
 %% API functions
 %%====================================================================
+
+% smpp
 unpack(#{short_message := SM0} = Pdu) ->
 	{UDH, SM} = split(SM0),
 	if map_size(UDH) > 1 -> Pdu#{short_message => SM, udh => UDH};
@@ -18,16 +20,51 @@ unpack(#{<<"short_message">> := SM0} = Pdu) ->
 					 end, #{}, UDH),
 	if map_size(UDH) > 1 -> Pdu#{<<"short_message">> => SM, <<"udh">> => UDH1};
 		true -> Pdu#{<<"short_message">> => SM}
-	end.
+	end;
+% ucp
+unpack(#{xser := XSERs} = Pdu) ->
+	case {udh_xser(XSERs, Pdu), Pdu} of
+		{NewPdu, #{xser := []}} -> NewPdu;
+		{#{xser := []} = NewPdu, Pdu} -> maps:without([xser], NewPdu);
+		{NewPdu, Pdu} -> NewPdu
+	end;
+unpack(#{<<"xser">> := XSERs} = Pdu) ->
+	case {udh_xser(XSERs, Pdu), Pdu} of
+		{NewPdu, #{<<"xser">> := []}} -> NewPdu;
+		{#{<<"xser">> := []} = NewPdu, Pdu} -> maps:without([<<"xser">>], NewPdu);
+		{NewPdu, Pdu} -> NewPdu
+	end;
+
+% everything else
+unpack(Pdu) -> Pdu.
 
 -define(HEADERS, [udh, <<"udh">>]).
 
+% smpp
 pack(#{udh := UDH, short_message := SM} = Pdu) ->
 	SM1 = unsplit(UDH, SM),
 	maps:without(?HEADERS, Pdu#{short_message => SM1});
 pack(#{<<"udh">> := UDH, <<"short_message">> := SM} = Pdu) ->
 	SM1 = unsplit(UDH, SM),
 	maps:without(?HEADERS, Pdu#{<<"short_message">> => SM1});
+
+% ucp
+pack(#{udh := UDH, xser := XSERs} = Pdu) ->
+	Data = unsplit(UDH, []),
+	maps:without(
+		?HEADERS,
+		Pdu#{xser => [#{type => 1, data => Data} | XSERs]}
+	);
+pack(#{<<"udh">> := UDH, <<"xser">> := XSERs} = Pdu) ->
+	Data = unsplit(UDH, []),
+	maps:without(
+		?HEADERS,
+		Pdu#{<<"xser">> => [#{<<"type">> => 1, <<"data">> => Data} | XSERs]}
+	);
+pack(#{udh := _, ot := _} = Pdu) -> pack(Pdu#{xser => []});
+pack(#{<<"udh">> := _, <<"ot">> := _} = Pdu) -> pack(Pdu#{<<"xser">> => []});
+
+% everything else
 pack(Pdu) -> Pdu.
 
 %%====================================================================
@@ -170,6 +207,21 @@ unsplit(#{bit := 16, len := 6, info_elm_id := 8, hdr_len := 4,
 			true 		-> <<6, 8, 4, R:16, C:8, N:8, SM/binary>>
 		end.
 
+udh_xser(XSERs, Pdu) ->
+	lists:foldl(
+		fun(#{type := 1, data := Data} = XSER, #{xser := NewXSREs} = PduMap) ->
+				{UDH, []} = split(Data),
+				PduMap#{udh => UDH, xser => NewXSREs -- [XSER]};
+			(#{<<"type">> := 1, <<"data">> := Data} = XSER,
+			 #{<<"xser">> := NewXSREs} = PduMap) ->
+				{UDH0, []} = split(Data),
+				UDH = maps:fold(fun(K, V, M) ->
+					 				M#{atom_to_binary(K, utf8) => V}
+					 			end, #{}, UDH0),
+				PduMap#{<<"udh">> => UDH, <<"xser">> => NewXSREs -- [XSER]};
+			(_, PduConst) -> PduConst
+		end, Pdu, XSERs).
+
 %------------------------------------------------------------------------------
 -ifdef(TEST).
 %%====================================================================
@@ -179,19 +231,20 @@ unsplit(#{bit := 16, len := 6, info_elm_id := 8, hdr_len := 4,
 -include_lib("eunit/include/eunit.hrl").
 
 -define(TESTS, [
-	{"no udh atom", #{short_message => [1]}, #{short_message => [1]}},
-	{"no udh bin", #{<<"short_message">> => <<1>>}, #{<<"short_message">> => <<1>>}},
-	{"udh 8bit atom",
+	% smpp
+	{"smpp no udh atom", #{short_message => [1]}, #{short_message => [1]}},
+	{"smpp no udh bin", #{<<"short_message">> => <<1>>}, #{<<"short_message">> => <<1>>}},
+	{"smpp udh 8bit atom",
 		#{udh => #{bit => 8, len => 5, info_elm_id => 0, hdr_len => 3,
 					msg_ref_num => 10, total_segments => 2,
 					segment_seqnum => 1}, short_message => [100]},
 		#{short_message => [5,0,3,10,2,1,100]}},
-	{"udh 8bit atom bin SM",
+	{"smpp udh 8bit atom bin SM",
 		#{udh => #{bit => 8, len => 5, info_elm_id => 0, hdr_len => 3,
 					msg_ref_num => 10, total_segments => 2,
 					segment_seqnum => 1}, short_message => <<100>>},
 		#{short_message => <<5,0,3,10,2,1,100>>}},
-	{"udh 8bit bin",
+	{"smpp udh 8bit bin",
 		#{<<"udh">> => #{<<"bit">> => 8, <<"len">> => 5,
 							<<"info_elm_id">> => 0, <<"hdr_len">> => 3,
 							<<"msg_ref_num">> => 10,
@@ -199,7 +252,7 @@ unsplit(#{bit := 16, len := 6, info_elm_id := 8, hdr_len := 4,
 							<<"segment_seqnum">> => 1},
 			<<"short_message">> => <<100>>},
 		#{<<"short_message">> => <<5,0,3,10,2,1,100>>}},
-	{"udh 8bit bin string SM",
+	{"smpp udh 8bit bin string SM",
 		#{<<"udh">> => #{<<"bit">> => 8, <<"len">> => 5,
 							<<"info_elm_id">> => 0, <<"hdr_len">> => 3,
 							<<"msg_ref_num">> => 10,
@@ -207,19 +260,19 @@ unsplit(#{bit := 16, len := 6, info_elm_id := 8, hdr_len := 4,
 							<<"segment_seqnum">> => 1},
 			<<"short_message">> => [100]},
 		#{<<"short_message">> => [5,0,3,10,2,1,100]}},
-	{"udh 16bit atom",
+	{"smpp udh 16bit atom",
 		#{udh => #{bit => 16, len => 6, info_elm_id => 8, hdr_len => 4,
 					msg_ref_num => [11,10], total_segments => 2,
 					segment_seqnum => 1},
 			short_message => [100]},
 		#{short_message => [6,8,4,11,10,2,1,100]}},
-	{"udh 16bit atom bin SM",
+	{"smpp udh 16bit atom bin SM",
 		#{udh => #{bit => 16, len => 6, info_elm_id => 8, hdr_len => 4,
 					msg_ref_num => 16#0B0A, total_segments => 2,
 					segment_seqnum => 1},
 			short_message => <<100>>},
 		#{short_message => <<6,8,4,11,10,2,1,100>>}},
-	{"udh 16bit bin",
+	{"smpp udh 16bit bin",
 		#{<<"udh">> => #{<<"bit">> => 16, <<"len">> => 6,
 							<<"info_elm_id">> => 8, <<"hdr_len">> => 4,
 							<<"msg_ref_num">> => 16#0B0A,
@@ -227,14 +280,39 @@ unsplit(#{bit := 16, len := 6, info_elm_id := 8, hdr_len := 4,
 							<<"segment_seqnum">> => 1},
 			<<"short_message">> => <<100>>},
 		#{<<"short_message">> => <<6,8,4,11,10,2,1,100>>}},
-	{"udh 16bit bin string SM",
+	{"smpp udh 16bit bin string SM",
 		#{<<"udh">> => #{<<"bit">> => 16, <<"len">> => 6,
 							<<"info_elm_id">> => 8, <<"hdr_len">> => 4,
 							<<"msg_ref_num">> => [11,10],
 							<<"total_segments">> => 2,
 							<<"segment_seqnum">> => 1},
 			<<"short_message">> => [100]},
-		#{<<"short_message">> => [6,8,4,11,10,2,1,100]}}
+		#{<<"short_message">> => [6,8,4,11,10,2,1,100]}},
+
+	% ucp
+	{"ucp no xser atom", #{ot => 52}, #{ot => 52}},
+	{"ucp no xser bin", #{<<"ot">> => 52}, #{<<"ot">> => 52}},
+	{"ucp empty xser atom", #{xser => []}, #{xser => []}},
+	{"ucp empty xser bin", #{<<"xser">> => []}, #{<<"xser">> => []}},
+	{"ucp xser but no udh atom",
+		#{xser => [#{type => 2, data => [0]}]},
+		#{xser => [#{type => 2, data => [0]}]}},
+	{"ucp xser but no udh bin",
+		#{<<"xser">> => [#{<<"type">> => 15, <<"data">> => [1]}]},
+		#{<<"xser">> => [#{<<"type">> => 15, <<"data">> => [1]}]}},
+	{"ucp udh atom without xser",
+		#{udh => #{bit => 8, len => 5, info_elm_id => 0, hdr_len => 3,
+				   msg_ref_num => 10, total_segments => 2,
+				   segment_seqnum => 1}, ot => 52},
+		#{xser => [#{type => 1, data => [5,0,3,10,2,1]}], ot => 52}},
+	{"ucp udh bin without xser",
+		#{<<"udh">> => #{<<"bit">> => 8, <<"len">> => 5,
+						 <<"info_elm_id">> => 0, <<"hdr_len">> => 3,
+						 <<"msg_ref_num">> => 10,
+						 <<"total_segments">> => 2,
+						 <<"segment_seqnum">> => 1}, <<"ot">> => 52},
+		#{<<"xser">> => [#{<<"type">> => 1, <<"data">> => [5,0,3,10,2,1]}],
+		  <<"ot">> => 52}}
 	]
 ).
 
