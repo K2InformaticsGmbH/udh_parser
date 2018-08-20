64fc123ac9e214b9d4fbcfbdde3593ff82989881
Bikram Chatterjee <razorpeak@gmail.com>
2018-08-20 09:29:00 +0200

    rafactored for readablity

--- src/udh_parser.erl
+++ src/udh_parser.erl
@@ -207,20 +207,19 @@ unsplit(#{bit := 16, len := 6, info_elm_id := 8, hdr_len := 4,
 			true 		-> <<6, 8, 4, R:16, C:8, N:8, SM/binary>>
 		end.
 
-udh_xser(XSERs, Pdu) ->
-	lists:foldl(
-		fun(#{type := 1, data := Data} = XSER, #{xser := NewXSREs} = PduMap) ->
-				{UDH, []} = split(Data),
-				PduMap#{udh => UDH, xser => NewXSREs -- [XSER]};
-			(#{<<"type">> := 1, <<"data">> := Data} = XSER,
-			 #{<<"xser">> := NewXSREs} = PduMap) ->
-				{UDH0, []} = split(Data),
-				UDH = maps:fold(fun(K, V, M) ->
-					 				M#{atom_to_binary(K, utf8) => V}
-					 			end, #{}, UDH0),
-				PduMap#{<<"udh">> => UDH, <<"xser">> => NewXSREs -- [XSER]};
-			(_, PduConst) -> PduConst
-		end, Pdu, XSERs).
+udh_xser(XSERs, Pdu) when is_list(XSERs) ->
+	lists:foldl(fun udh_xser/2, Pdu, XSERs);
+udh_xser(#{type := 1, data := Data} = XSER, #{xser := NewXSREs} = PduMap) ->
+	{UDH, []} = split(Data),
+	PduMap#{udh => UDH, xser => NewXSREs -- [XSER]};
+udh_xser(#{<<"type">> := 1, <<"data">> := Data} = XSER,
+			#{<<"xser">> := NewXSREs} = PduMap) ->
+	{UDH0, []} = split(Data),
+	UDH = maps:fold(fun(K, V, M) ->
+						M#{atom_to_binary(K, utf8) => V}
+					end, #{}, UDH0),
+	PduMap#{<<"udh">> => UDH, <<"xser">> => NewXSREs -- [XSER]};
+udh_xser(_, PduConst) -> PduConst.
 
 %------------------------------------------------------------------------------
 -ifdef(TEST).
